529b50c1098c9d7c3374f26fde172e751a4c8404
Mikhail <miksolodovnikov@gmail.com>
2017-01-10 20:30:30 +0500

    Add solution for permutation problem, which crate hash map of characters with it counter for both words and compare this words. The creation of map is linear in time and space. Hash map comparision is also linear in time.

--- src/main/StringAnalyser.java
+++ src/main/StringAnalyser.java
@@ -1,5 +1,6 @@
 package main;
 
+import java.util.HashMap;
 import java.util.HashSet;
 
 class StringAnalyser {
@@ -26,6 +27,23 @@ class StringAnalyser {
     }
 
     static Boolean isPermutation(String first, String second) {
-        return false;
+        if (first.length() != second.length()) return false;
+
+        HashMap<Character, Integer> hf = countChars(first);
+        HashMap<Character, Integer> hs = countChars(second);
+        return hf.equals(hs);
+    }
+
+    private static HashMap<Character,Integer> countChars(String s) {
+        HashMap<Character, Integer> res = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            Character c = s.charAt(i);
+            if (res.containsKey(c)) {
+                res.put(c, res.get(c) + 1);
+            } else {
+                res.put(c, 0);
+            }
+        }
+        return res;
     }
 }
--- test/main/StringAnalyserTest.java
+++ test/main/StringAnalyserTest.java
@@ -10,7 +10,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class StringAnalyserTest {
     private List<Map.Entry<String, Boolean>> isUniqueTestCases;
-    private List<Map.Entry<Pair<String, String>, Boolean>> isPermutationTestCases;
+    private List<Pair<Pair<String, String>, Boolean>> isPermutationTestCases;
 
     @BeforeEach
     void setUp() {
@@ -19,9 +19,9 @@ class StringAnalyserTest {
                 new AbstractMap.SimpleEntry<>("ABCDEFG", true),
                 new AbstractMap.SimpleEntry<>("ABCDEFGG", false),
                 new AbstractMap.SimpleEntry<>("AA", false));
-        Collections.addAll(isPermutationTestCases,
-                new AbstractMap.SimpleEntry<>(new Pair<>("qwerty", "ytrewq"), true),
-                new AbstractMap.SimpleEntry<>(new Pair<>("abcd", "abcc"), false));
+        isPermutationTestCases = new ArrayList<>();
+        isPermutationTestCases.add(new Pair<>(new Pair<>("qwerty", "ytrewq"), true));
+        isPermutationTestCases.add(new Pair<>(new Pair<>("abcd", "abcc"), false));
     }
 
     @Test
@@ -38,9 +38,10 @@ class StringAnalyserTest {
 
     @Test
     void isPermutation() {
-        for (Map.Entry<Pair<String, String>, Boolean> t : isPermutationTestCases)
-            assertEquals(t.getValue(), StringAnalyser.isPermutation(t.getKey().first(),
-                    t.getKey().second()));
+        for (Pair<Pair<String, String>, Boolean> t : isPermutationTestCases) {
+            assertEquals(t.second(), StringAnalyser.isPermutation(t.first().first(),
+                    t.first().second()));
+        }
     }
 
 }
