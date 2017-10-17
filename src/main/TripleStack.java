2ff8d86e533bf43da34c61cb16e2895dd2e06a3f
Mikhail <miksolodovnikov@gmail.com>
2017-10-17 22:01:11 +0500

    Add solution for task 3.1. Implement stacks of fixed sizes on single array and full api for stack data structure.

--- src/main/TripleStack.java
+++ src/main/TripleStack.java
@@ -1,4 +1,64 @@
 package main;
 
-public class TripleStack {
+import java.util.EmptyStackException;
+
+class TripleStack {
+    private final int numberOfStacks = 3;
+    private int capacity;
+    private int[] sizes;
+    private int[] values;
+
+    TripleStack(int c) {
+        capacity = c;
+        sizes = new int[numberOfStacks];
+        values = new int[numberOfStacks * capacity];
+    }
+
+    int getNumberOfStacks() {
+        return numberOfStacks;
+    }
+
+    int getCapacity() {
+        return capacity;
+    }
+
+    int pop(int stackNumber) throws EmptyStackException {
+        if (isEmpty(stackNumber)) {
+            throw new EmptyStackException();
+        }
+        int topIdx = indexOfTop(stackNumber);
+        int value = values[topIdx];
+        values[topIdx] = 0;
+        sizes[stackNumber]--;
+        return value;
+    }
+
+    boolean isEmpty(int stackNumber) {
+        return sizes[stackNumber] == 0;
+    }
+
+    void push(int value, int stackNumber) throws StackOverflowError {
+        if (isFull(stackNumber)) {
+            throw new StackOverflowError("Stack is full.");
+        }
+        sizes[stackNumber]++;
+        values[indexOfTop(stackNumber)] = value;
+    }
+
+    boolean isFull(int stackNumber) {
+        return sizes[stackNumber] >= capacity;
+    }
+
+    private int indexOfTop(int stackNumber) {
+        int offset = stackNumber * capacity;
+        int size = sizes[stackNumber];
+        return offset + size - 1;
+    }
+
+    int peek(int stackNumber) throws EmptyStackException {
+        if (isEmpty(stackNumber)) {
+            throw new EmptyStackException();
+        }
+        return values[indexOfTop(stackNumber)];
+    }
 }
--- test/main/StackProblemTest.java
+++ test/main/StackProblemTest.java
@@ -1,14 +1,100 @@
 package main;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.EmptyStackException;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class StackProblemTest {
+    private final int capacity = 100;
+    private TripleStack s;
+
+    @BeforeEach
+    void setUp() {
+        s = new TripleStack(capacity);
+    }
 
     @Test
-    void test_new_triple_stack_class() throws Exception {
-        TripleStack s = new TripleStack();
+    void testNewTripleStackClass() throws Exception {
         assertTrue(s instanceof TripleStack);
+    }
+
+    @Test
+    void testTripleStackNumbersOfStack() throws Exception {
+        assertEquals(3, s.getNumberOfStacks());
+    }
+
+    @Test
+    void testTripleStackCapacity() throws Exception {
+        assertEquals(capacity, s.getCapacity());
+    }
+
+    @Test
+    void testStackIsEmpty() throws Exception {
+        for (int stackNumber = 0; stackNumber < 3; stackNumber++) {
+            assertTrue(s.isEmpty(stackNumber));
+        }
+
+    }
+
+    @Test
+    void testPopEmptyStack() throws Exception {
+        for (int stackNumber = 0; stackNumber < 3; stackNumber++) {
+            int finalStackNumber = stackNumber;
+            assertThrows(EmptyStackException.class, () -> s.pop(finalStackNumber));
+        }
+    }
+
+    @Test
+    void testPushToStacks() throws Exception {
+        int v = 1;
+
+        for (int stackNumber = 0; stackNumber < 3; stackNumber++) {
+            s.push(v, stackNumber);
+            assertFalse(s.isEmpty(stackNumber));
+        }
+    }
+
+    @Test
+    void testPopNonEmptyStack() throws Exception {
+        for (int stackNumber = 0; stackNumber < 3; stackNumber++) {
+            s.push(stackNumber, stackNumber);
+            assertEquals(stackNumber, s.pop(stackNumber));
+        }
+    }
+
+    @Test
+    void testStackIsFull() throws Exception {
+        for (int i = 0; i < s.getCapacity(); i++) {
+            s.push(i, 0);
+        }
+        assertTrue(s.isFull(0));
+    }
+
+    @Test
+    void testOverflowStackCapacity() throws Exception {
+        for (int i = 0; i < s.getCapacity(); i++) {
+            s.push(i, 0);
+        }
+        assertThrows(StackOverflowError.class, () -> s.push(0,0));
+    }
+
+    @Test
+    void testPeekOnEmptyStack() throws Exception {
+        for (int i = 0; i < 3; i++) {
+            int stackNumber = i;
+            assertThrows(EmptyStackException.class, () -> s.peek(stackNumber));
+        }
+    }
+
+    @Test
+    void testPeekOnNonEmptyStack() throws Exception {
+        for (int i = 0; i < 3; i++) {
+            s.push(i, i);
+            assertEquals(i, s.peek(i));
+            assertFalse(s.isEmpty(i));
+        }
     }
 }
