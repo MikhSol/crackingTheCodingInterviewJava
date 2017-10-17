78269cdb4b64ae53fb9e7a8394bc6495ead7a61c
Mikhail <miksolodovnikov@gmail.com>
2017-10-17 19:55:27 +0500

    Add boilerplate for solution to 3.1 task (stacks).

--- test/main/StackProblemTest.java
+++ test/main/StackProblemTest.java
@@ -1,4 +1,14 @@
 package main;
 
-public class StackProblemTest {
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class StackProblemTest {
+
+    @Test
+    void test_new_triple_stack_class() throws Exception {
+        TripleStack s = new TripleStack();
+        assertTrue(s instanceof TripleStack);
+    }
 }
