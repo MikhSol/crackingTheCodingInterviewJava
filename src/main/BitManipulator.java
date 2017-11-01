9c2412925d5b318cc076eb9d5756cdc8bb0ef14d
Mikhail <miksolodovnikov@gmail.com>
2017-11-01 15:47:12 +0500

    Add solution for task 5.2 - print decimal number less than 1 and grater than 0 in binary format.

--- src/main/BitManipulator.java
+++ src/main/BitManipulator.java
@@ -1,7 +1,33 @@
 package main;
 
+import static java.lang.Math.pow;
+
 class BitManipulator {
     static int insertBitsToPosition(int n, int m, int i, int j) {
         return (n & ((~0 << j) | ((1 << i-1) - 1))) | (m << i-1);
+    }
+
+    static String doubleAsBinString(double in) {
+        if (in >= 1 || in <= 0) return "Error";
+        StringBuilder result = new StringBuilder();
+        result.append(".");
+        int power = -1;
+        while (in > 0) {
+            if (result.length() > 32) return "Error";
+            double residual = in - pow(2, power);
+            if (residual > 0) {
+                in = residual;
+                result.append("1");
+            } else if (residual == 0) {
+                result.append("1");
+                break;
+            } else if (residual < 0) {
+                result.append("0");
+                power--;
+                continue;
+            }
+            power--;
+        }
+        return result.toString();
     }
 }
--- test/main/BitManipulatorTest.java
+++ test/main/BitManipulatorTest.java
@@ -15,4 +15,16 @@ class BitManipulatorTest {
         int Output = 0b1000100110;
         assertEquals(Output, BitManipulator.insertBitsToPosition(N, M, i, j));
     }
+
+    @Test
+    void testBinaryRepresentationOfDoubleFromZeroToOne() {
+        assertEquals("Error", BitManipulator.doubleAsBinString(1.5));
+
+        assertEquals("Error", BitManipulator.doubleAsBinString(-0.2));
+
+        // In = 0.75
+        assertEquals(".11", BitManipulator.doubleAsBinString(.5*1 + .5*.5 *1));
+
+        assertEquals(".1101", BitManipulator.doubleAsBinString(.5*1 + .5*.5*1 + .5*.5*.5*0 + .5*.5*.5*.5*1));
+    }
 }
